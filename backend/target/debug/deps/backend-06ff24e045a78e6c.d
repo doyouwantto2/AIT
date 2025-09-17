d998328d4ddb314fd352c8a25862706d1c2f4950
doyouwantto2 <92518098+doyouwantto2@users.noreply.github.com>
2025-09-17 17:00:47 +0700

    Initial commit

---
+++ backend/target/debug/deps/backend-06ff24e045a78e6c.d
@@ -0,0 +1,9 @@
+/home/emiya2467/Documents/Interface/Framework/AIT/backend/target/debug/deps/backend-06ff24e045a78e6c.d: src/main.rs Cargo.toml
+
+/home/emiya2467/Documents/Interface/Framework/AIT/backend/target/debug/deps/libbackend-06ff24e045a78e6c.rmeta: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
